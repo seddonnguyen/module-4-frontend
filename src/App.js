74c8f9b40edab42c00a1c590396a351d2682f199
github username <github email address>
2019-08-20 12:08:47 -0600

    Add edit and delete

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import FugitiveCard from './components/FugitiveCard'
+import EditFugitive from './components/EditFugitive'
 
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      fugitives: []
+      fugitives: [],
+      fugitive: {}
     }
   }
 
@@ -20,32 +22,67 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       this.setState({
-        fugitives: response
+        fugitives: response,
+        fugitive: {},
+        newFugitive: false
       })
     })
   }
 
-  updateFugitives = () => {
-    fetch('http://localhost:3000/fugitives')
-    .then(response => response.json())
-    .then(response => {
-      this.setState({
-        fugitives: response
-      })
+
+  editFugitive = (fugitive) => {
+    this.setState({
+      fugitive
     })
   }
 
   displayFugitives = () => {
-    return this.state.fugitives.map(fugitive => <FugitiveCard key={fugitive.id} fugitive={fugitive} updateFugitives={this.updateFugitives}/>)
+    return this.state.fugitives.map(fugitive => <FugitiveCard
+                                                  key={fugitive.id}
+                                                  fugitive={fugitive}
+                                                  updateFugitives={this.resetFugitive}
+                                                  editFugitive={this.editFugitive}
+                                                />)
+  }
+
+  resetFugitive = () => {
+    fetch('http://localhost:3000/fugitives')
+    .then(response => response.json())
+    .then(response => {
+      this.setState({
+        fugitives: response,
+        fugitive: {},
+        newFugitive: false
+      })
+    })
+  }
+
+  displayView = () => {
+    if(!this.state.fugitive.id && !this.state.newFugitive) {
+      return (
+              <div className="card-deck">
+                {this.displayFugitives()}
+              </div>
+              );
+    }
+    else if (this.state.newFugitive) {
+
+    }
+    else {
+          console.log(this.state.fugitive)
+      return <EditFugitive
+                fugitive={this.state.fugitive}
+                backToMain={this.resetFugitive}
+                updateFugitive={this.resetFugitive}
+              />
+    }
   }
 
   render() {
     return (
       <div className="container">
         <h1>Most Wanted</h1>
-        <div className="card-deck">
-        {this.displayFugitives()}
-        </div>
+        {this.displayView()}
       </div>
     );
   }
--- src/components/FugitiveCard.js
+++ src/components/FugitiveCard.js
@@ -26,13 +26,13 @@ export default class FugitiveCard extends Component {
       );
   }
 
-  handleChargeChange = (event) => {
+  handleChargeChange = event => {
     this.setState({
       charge: event.target.value
     });
   }
 
-  handleChargeSubmit = (event) => {
+  handleChargeSubmit = event => {
     const url = "http://localhost:3000/charges"
 
     event.preventDefault();
@@ -56,13 +56,26 @@ export default class FugitiveCard extends Component {
     })
   }
 
-  handleAliasChange = (event) => {
+  deleteFugitive = event => {
+    const url = "http://localhost:3000/fugitives"
+
+    fetch(`${url}/${this.props.fugitive.id}`, {
+      method: "DELETE",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      this.props.updateFugitives()
+    })
+  }
+
+  handleAliasChange = event => {
     this.setState({
       alias: event.target.value
     });
   }
 
-  handleAliasSubmit = (event) => {
+  handleAliasSubmit = event => {
     const url = "http://localhost:3000/aliases"
 
     event.preventDefault();
@@ -87,40 +100,46 @@ export default class FugitiveCard extends Component {
   }
 
   render() {
-    const { fugitive } = this.props
+    const { fugitive, editFugitive } = this.props
 
     return (
-      <div className="card">
-        <div className="card-header">
-          <h4>{fugitive.name}</h4>
-        </div>
+      <div className="row">
+      <div className="col-sm-12">
+        <div className="card">
+          <div className="card-header">
+            <h4>{fugitive.name}</h4>
+          </div>
 
-        <img src={fugitive.image} className="card-img-top" alt=""/>
-        <div className="card-body" key={fugitive.id}>
-          <p className="card-text"> Charge(s):</p>
-          {this.displayCharges()}
-          <div className="form-group">
-            <form className="form-inline" onSubmit={this.handleChargeSubmit}>
-              <input type="text" className="form-control" value={this.state.charge} onChange={this.handleChargeChange} />
-              <button type="submit" className="btn btn-primary m-2">Add</button>
-            </form>
+          <img src={fugitive.image} className="card-img-top" alt=""/>
+          <div className="card-body" key={fugitive.id}>
+            <button onClick={() => editFugitive(fugitive)} className="btn btn-primary m-2">Edit</button>
+            <button onClick={this.deleteFugitive} className="btn btn-primary m-2">Delete</button>
+            <p className="card-text"> Charge(s):</p>
+            {this.displayCharges()}
+            <div className="form-group">
+              <form className="form-inline" onSubmit={this.handleChargeSubmit}>
+                <input type="text" className="form-control" value={this.state.charge} onChange={this.handleChargeChange} />
+                <button type="submit" className="btn btn-primary m-2">Add</button>
+              </form>
+            </div>
+            <p className="card-text"> Aliases:</p>
+             {this.displayAliases()}
+            <div className="form-group">
+              <form className="form-inline" onSubmit={this.handleAliasSubmit}>
+                <input type="text" className="form-control" value={this.state.alias} onChange={this.handleAliasChange} />
+                <button type="submit" className="btn btn-primary m-2">Add</button>
+              </form>
+            </div>
+            <p className="card-text"> Height: {fugitive.height}</p>
+            <p className="card-text"> Weight: {fugitive.weight}</p>
+            <p className="card-text"> Race: {fugitive.race}</p>
+            <p className="card-text"> Hair: {fugitive.hair}</p>
+            <p className="card-text"> Eyes: {fugitive.eyes}</p>
+            <p className="card-text"> Sex: {fugitive.sex}</p>
+            <p className="card-text"> Captured: {fugitive.captured}</p>
           </div>
-          <p className="card-text"> Aliases:</p>
-           {this.displayAliases()}
-          <div className="form-group">
-            <form className="form-inline" onSubmit={this.handleAliasSubmit}>
-              <input type="text" className="form-control" value={this.state.alias} onChange={this.handleAliasChange} />
-              <button type="submit" className="btn btn-primary m-2">Add</button>
-            </form>
-          </div>
-          <p className="card-text"> Height: {fugitive.height}</p>
-          <p className="card-text"> Weight: {fugitive.weight}</p>
-          <p className="card-text"> Race: {fugitive.race}</p>
-          <p className="card-text"> Hair: {fugitive.hair}</p>
-          <p className="card-text"> Eyes: {fugitive.eyes}</p>
-          <p className="card-text"> Sex: {fugitive.sex}</p>
-          <p className="card-text"> Captured: {fugitive.captured}</p>
         </div>
+      </div>
       </div>
     );
   }
